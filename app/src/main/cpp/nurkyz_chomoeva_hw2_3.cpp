95ebae364870b47681321c2c0b55f1d6a62f6d98
Nurkyz03 <chomoevanurkyz03@gmail.com>
2024-07-12 11:51:40 +0600

    LinearLayout

---
+++ app/src/main/cpp/nurkyz_chomoeva_hw2_3.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("nurkyz_chomoeva_hw2_3");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("nurkyz_chomoeva_hw2_3")
+//      }
+//    }
